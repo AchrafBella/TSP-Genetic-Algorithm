ec49dcb1cb17a83ffa91e3510a7638da40b53397
SuperNova <52492864+loverman1@users.noreply.github.com>
2020-05-07 23:10:11 +0200

    Update TSP_Alg_Gen.py

--- TSP_Alg_Gen.py
+++ TSP_Alg_Gen.py
@@ -14,19 +14,6 @@ from matplotlib import pyplot as plt
 from Rout import Rout
 from City import City
 
-"""
-TSP : Given a list of cities and the distances between each pair of cities
-     what is the shortest possible route that visits each city and returns to the origin city?
-Genetic Algorithm approach:
-    Gene       : a city (represented as (x, y) coordinates)
-    Individual : a single route satisfying the conditions above
-    Population : a collection of possible routes (i.e., collection of individuals)
-    Parents    : two routes that are combined to create a new route
-    Mating pool: a collection of parents that are used to create our next population (thus creating the next generation of routes)
-    Fitness    : a function that tells us how good each route is (in our case, how short the distance is)
-    Mutation   : a way to introduce variation in our population by randomly swapping two cities in a route
-    Elitism    : a way to carry the best individuals into the next generation
-"""
 
 # creation of a collection on induvidual where every individual is a possible rout 
 # pop == popualtion of routes
@@ -48,7 +35,7 @@ def Selection(pop):
 # each parent is a tour 
 # and we gonna take some cities in parent1 (tour1) and take the rest from parent 2 and making sure that the cityes aren't the same
 
-def beerd(parent1 , parent2):
+def merge(parent1 , parent2):
     p1 = []
     geneA = int(random.random() * len(parent1.getCities()))
     geneB = int(random.random() * len(parent1.getCities()))
@@ -79,7 +66,7 @@ def crossover(pop, top):
             parent2 = top_10[np.random.randint(top)]
             pass
         
-        kid = beerd(parent1, parent2)
+        kid = merge(parent1, parent2)
         children.append(kid)
     pop.extend(children)
     return pop
